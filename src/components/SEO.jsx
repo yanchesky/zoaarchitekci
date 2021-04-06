877627fe05afdfca99d53424a578c6a9fa6b07d8
Janek Biardzki <jan.biardzki@netinteractive.pl>
2021-04-06 19:48:10 +0200

    2

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,37 +3,39 @@ import { useLocation } from "@reach/router";
 import { Helmet } from "react-helmet";
 import { useIntl } from "gatsby-plugin-intl";
 
+const SITE_URL = "https://zoa-architekci.web.app";
+
 const SEO = ({ title, description, image }) => {
   const intl = useIntl();
   const { pathname } = useLocation();
   console.log("pathname:", pathname);
   console.log("image:", image);
   return (
-    <Helmet titleTemplate="%s | ZOA ARCHITEKCI">
-      <html dir="ltr" lang={intl.locale} />
-      <meta charSet="utf-8" />
-      <title lang={intl.locale}>{title}</title>
-      <meta name="description" content={description} />
+    <>
+      <Helmet titleTemplate="%s | ZOA ARCHITEKCI">
+        <html dir="ltr" lang={intl.locale} />
+        <meta charSet="utf-8" />
+        <title lang={intl.locale}>{title}</title>
+        <link rel="canonical" href={`${SITE_URL}${pathname}`} />
+        <meta name="description" content={description} />
+      </Helmet>
       {image ? (
         <Helmet>
           <meta property="og:type" content="website" />
-          <meta
-            property="og:url"
-            content={`https://zoa-architekci.web.app${pathname}`}
-          />
+          <meta property="og:url" content={`${SITE_URL}${pathname}`} />
           <meta property="og:title" content={title} />
           <meta
             property="og:image"
-            content={`https://zoa-architekci.web.app/${image.childImageSharp.fixed.src}`}
+            content={`${SITE_URL}${image.childImageSharp.fixed.src}`}
           />
           <meta property="og:description" content={description} />
           <meta property="og:site_name" content="ZOA ARCHITEKCI" />
           <meta property="og:locale" content={intl.locale} />
-          <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
+          <meta property="og:image:width" content="600" />
+          <meta property="og:image:height" content="315" />
         </Helmet>
       ) : null}
-    </Helmet>
+    </>
   );
 };
 
